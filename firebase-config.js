9f4aa9bfa30e52f85af09587dee8112d510b3802
lost <lost@test.com>
2026-03-20 20:53:29 +0800

    v1.0.3 - 添加 Firebase 云端存储支持

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase 配置
+const firebaseConfig = {
+  apiKey: "AIzaSyBHmZ5-322j46o77fd46cWfuiRu8gsKonc",
+  authDomain: "test001-f643a.firebaseapp.com",
+  databaseURL: "https://test001-f643a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "test001-f643a",
+  storageBucket: "test001-f643a.firebasestorage.app",
+  messagingSenderId: "230236318931",
+  appId: "1:230236318931:web:56782ff20c1589c09d7826"
+};
